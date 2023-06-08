522d1bd73dca6cebe1a0045f8201d45aaf4f3d58
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2023-06-08 23:11:12 +0100

    Attaching some JS after quiescence

---
+++ src/StatefulReconnection/Enable.razor.js
@@ -0,0 +1,7 @@
+﻿export function init() {
+    const origOnConnectionDown = Blazor.defaultReconnectionHandler.onConnectionDown;
+
+    Blazor.defaultReconnectionHandler.onConnectionDown = function() {
+        return origOnConnectionDown.apply(this, arguments);
+    }
+}
